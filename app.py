4f9ed480d78c1826652edcd9bd20dc8427202118
seungwooJoo <thisis.joos@gmail.com>
2022-05-12 11:49:10 +0900

    status bar

--- app.py
+++ app.py
@@ -1,23 +1,29 @@
 import sys
-from PyQt6.QtWidgets import QApplication, QWidget, QPushButton, QToolTip
+from PyQt6.QtWidgets import QApplication, QWidget, QPushButton, QToolTip, QMainWindow
 from PyQt6.QtGui import QIcon
 from PyQt6.QtCore import QCoreApplication
 from PyQt6.QtGui import QFont
 
-class MyApp(QWidget):
+
+class MyApp(QMainWindow):
 
     def __init__(self):
         super().__init__()
         self.initUI()
 
     def initUI(self):
+        # 툴팁
         QToolTip.setFont(QFont('SansSerif', 10))
         self.setToolTip('This is <b>QWidhet</b> widget')
         
+        # 버튼
         btn = QPushButton('Quit', self) # (버튼에 표시될 텍스트, 버튼이 위치할 부모 위젯)
         btn.move(50, 50)
         btn.resize(btn.sizeHint())
         btn.clicked.connect(QCoreApplication.instance().quit)
+        
+        # 상태바
+        self.statusBar().showMessage('Ready')
         
         self.setWindowTitle('My First Application')
         self.setWindowIcon(QIcon('web.png'))    # 타이틀바의 아이콘
