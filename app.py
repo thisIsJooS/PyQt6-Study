fe354f754c73656f6f85cee299feae3b49904ca9
seungwooJoo <thisis.joos@gmail.com>
2022-05-12 12:20:45 +0900

    toolbar

--- app.py
+++ app.py
@@ -17,28 +17,42 @@ class MyApp(QMainWindow):
         
         # 버튼
         btn = QPushButton('Quit', self) # (버튼에 표시될 텍스트, 버튼이 위치할 부모 위젯)
-        btn.move(50, 50)
+        btn.move(300, 300)
         btn.resize(btn.sizeHint())
         btn.clicked.connect(QCoreApplication.instance().quit)
         
         # 상태바
         self.statusBar().showMessage('Ready')
         
-        # 메뉴바
+        # 메뉴바 - 어플리케이션에서 사용되는 모든 명령의 모음
+        saveAction = QAction(QIcon('save.png'), 'Save', self)
+        editAction = QAction(QIcon('edit.png'), 'Edit', self)
+        printAction = QAction(QIcon('print.png'), 'Print', self)
         exitAction = QAction(QIcon('exit.png'), 'Exit', self)
         exitAction.setShortcut('Ctrl+Q')
         exitAction.setStatusTip('Exit application')
         exitAction.triggered.connect(QApplication.quit)
+        
         menubar = self.menuBar()
         menubar.setNativeMenuBar(False)
         filemenu = menubar.addMenu('&File')
         filemenu.addAction(exitAction)
+        filemenu.addAction(saveAction)
+        editmenu = menubar.addMenu('&Edit')
+        editmenu.addAction(editAction)
+        editmenu.addAction(printAction)
 
+        # 툴바 - 자주 사용하는 명령들을 툴바에 표기
+        self.toolbar = self.addToolBar('Exit')
+        self.toolbar.addAction(saveAction)
+        self.toolbar.addAction(editAction)
+        self.toolbar.addAction(printAction)
+        self.toolbar.addAction(exitAction)
         
         # Main
         self.setWindowTitle('My First Application')
         self.setWindowIcon(QIcon('web.png'))    # 타이틀바의 아이콘
-        self.setGeometry(300, 300, 300, 200)    # move()와 resize()를 하나로 합친 메서드
+        self.setGeometry(400, 400, 600, 400)    # move()와 resize()를 하나로 합친 메서드
         self.show()
 
 
