759798cd6b4654a84b6e1c86a1eb3003270fcb8f
seungwooJoo <thisis.joos@gmail.com>
2022-05-13 15:43:07 +0900

    QGroupBox

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
 import sys
-from PyQt6.QtWidgets import QApplication, QWidget, QPushButton, QToolTip, QMainWindow
-from PyQt6.QtWidgets import QLabel, QGridLayout, QLabel, QLineEdit
-
+from tokenize import group
+from PyQt6.QtWidgets import *
 from PyQt6.QtGui import QIcon, QAction, QFont, QGuiApplication
-from PyQt6.QtCore import QCoreApplication, QDateTime, Qt, QBasicTimer
+from PyQt6.QtCore import QCoreApplication, QDateTime, Qt
 
 class MyApp(QMainWindow):
 
@@ -22,12 +21,6 @@ class MyApp(QMainWindow):
         # 툴팁
         QToolTip.setFont(QFont('SansSerif', 10))
         self.setToolTip('This is <b>QWidget</b> widget')
-        
-        # 버튼
-        # btn = QPushButton('Quit', self) # (버튼에 표시될 텍스트, 버튼이 위치할 부모 위젯)
-        # btn.move(300, 300)
-        # btn.resize(btn.sizeHint())
-        # btn.clicked.connect(QCoreApplication.instance().quit)
         
         # 상태바
         self.statusBar().showMessage(QDateTime.currentDateTime().toString())
@@ -57,22 +50,106 @@ class MyApp(QMainWindow):
         self.toolbar.addAction(printAction)
         self.toolbar.addAction(exitAction)
         
-        # QLineEdit
-        self.lbl = QLabel(self)
-        self.lbl.move(300, 400)
-
-        qle = QLineEdit(self)
-        qle.move(300, 300)
-        qle.textChanged[str].connect(self.onChanged)
+        # QGroupBox
+        grid = QGridLayout()
+        grid.addWidget(self.createFirstExclusiveGroup(), 0, 0)
+        grid.addWidget(self.createSecondExclusiveGroup(), 1, 0)
+        grid.addWidget(self.createNonExclusiveGroup(), 0, 1)
+        grid.addWidget(self.createPushButtonGroup(), 1, 1)
+        
+        widget.setLayout(grid)
         
         # Main
         self.show()
 
+    
+    def createFirstExclusiveGroup(self):
+        groupbox = QGroupBox('Exclusive Radio Buttons')
+        
+        radio1 = QRadioButton('Radio1')
+        radio2 = QRadioButton('Radio2')
+        radio3 = QRadioButton('Radio2')
+        radio1.setChecked(True)
 
-    def onChanged(self, text):
-        self.lbl.setText(text)
-        self.lbl.adjustSize()
+        vbox = QVBoxLayout()
+        vbox.addWidget(radio1)
+        vbox.addWidget(radio2)
+        vbox.addWidget(radio3)
+        groupbox.setLayout(vbox)
+        
+        return groupbox
+        
+        
+    def createSecondExclusiveGroup(self):
+        groupbox = QGroupBox('Exclusive Radio Buttons')
+        groupbox.setCheckable(True)
+        groupbox.setChecked(False)
+        
+        radio1 = QRadioButton('Radio1')
+        radio2 = QRadioButton('Radio2')
+        radio3 = QRadioButton('Radio3')
+        radio1.setChecked(True)
+        checkbox = QCheckBox('Independent Checkbox')
+        checkbox.setChecked(True)
+        
+        vbox = QVBoxLayout()
+        vbox.addWidget(radio1)
+        vbox.addWidget(radio2)
+        vbox.addWidget(radio3)
+        vbox.addWidget(checkbox)
+        vbox.addStretch(1)
+        groupbox.setLayout(vbox)
 
+        return groupbox
+    
+        
+    def createNonExclusiveGroup(self):
+        groupbox = QGroupBox('Non-Exclusive Checkboxes')
+        groupbox.setFlat(True)
+
+        checkbox1 = QCheckBox('CheckBox1')
+        checkbox2 = QCheckBox('CheckBox2')
+        checkbox2.setChecked(True)
+        tristatebox = QCheckBox('Tri-state Button')
+        tristatebox.setTristate(True)
+
+        vbox = QVBoxLayout()
+        vbox.addWidget(checkbox1)
+        vbox.addWidget(checkbox2)
+        vbox.addWidget(tristatebox)
+        vbox.addStretch(1)
+        groupbox.setLayout(vbox)
+        
+        return groupbox
+        
+    def createPushButtonGroup(self):
+        groupbox = QGroupBox('Push Buttons')
+        groupbox.setCheckable(True)
+        groupbox.setChecked(True)
+
+        pushbutton = QPushButton('Normal Button')
+        togglebutton = QPushButton('Toggle Button')
+        togglebutton.setCheckable(True)
+        togglebutton.setChecked(True)
+        flatbutton = QPushButton('Flat Button')
+        flatbutton.setFlat(True)
+        popupbutton = QPushButton('Popup Button')
+        menu = QMenu(self)
+        menu.addAction('First Item')
+        menu.addAction('Second Item')
+        menu.addAction('Third Item')
+        menu.addAction('Fourth Item')
+        popupbutton.setMenu(menu)
+        
+        vbox = QVBoxLayout()
+        vbox.addWidget(pushbutton)
+        vbox.addWidget(togglebutton)
+        vbox.addWidget(flatbutton)
+        vbox.addWidget(popupbutton)
+        vbox.addStretch(1)
+        groupbox.setLayout(vbox)
+
+        return groupbox
 
     def center(self):
         qr = self.frameGeometry()   # 스크린의 위치와 크기 정보를 가져옴
